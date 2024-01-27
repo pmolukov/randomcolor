89cb403ca45c770defc743e2d66ee63ed401356a
Pavel Molukov <p.molukov@gmail.com>
2024-01-27 20:14:08 +0300

    well done

---
+++ script.js
@@ -0,0 +1,97 @@
+const cols = document.querySelectorAll(".col");
+
+document.addEventListener("keydown", (event) => {
+  event.preventDefault();
+  if (event.code.toLowerCase() === "space") {
+    setRandomColors();
+  }
+});
+
+document.addEventListener("click", (event) => {
+  const type = event.target.dataset.type;
+
+  if (type === "lock") {
+    const node =
+      event.target.tagName.toLowerCase() === "i"
+        ? event.target
+        : event.target.children[0];
+
+    node.classList.toggle("fa-lock-open");
+    node.classList.toggle("fa-lock");
+  } else if (type === "copy") {
+    copyToClickboard(event.target.textContent);
+  } else if (type === "change") {
+    setRandomColors();
+  }
+});
+
+function copyToClickboard(text) {
+  return navigator.clipboard.writeText(text);
+}
+
+function setRandomColors(isInitial) {
+  const colors = isInitial ? getColorsFromHash() : [];
+
+  cols.forEach((col, index) => {
+    const islocked = col.querySelector("i").classList.contains("fa-lock");
+    const text = col.querySelector("h2");
+    const button = col.querySelector("button");
+
+    if (islocked) {
+      colors.push(text.textContent);
+      return;
+    }
+
+    const color = isInitial
+      ? colors[index]
+        ? colors[index]
+        : chroma.random()
+      : chroma.random();
+
+    if (!isInitial) {
+      colors.push(color);
+    }
+
+    text.textContent = color;
+    col.style.background = color;
+
+    setTextColor(text, color);
+    setTextColor(button, color);
+  });
+
+  updateColorsHash(colors);
+}
+
+function setTextColor(text, color) {
+  const luminence = chroma(color).luminance();
+  text.style.color = luminence > 0.5 ? "black" : "white";
+}
+
+function updateColorsHash(colors = []) {
+  document.location.hash = colors
+    .map((col) => col.toString().substring(1))
+    .join("-");
+}
+
+function getColorsFromHash() {
+  if (document.location.hash.length > 1) {
+    return document.location.hash
+      .substring(1)
+      .split("-")
+      .map((color) => "#" + color);
+  }
+  return [];
+}
+
+setRandomColors(true);
+
+// function generateRandomColor() {
+//   const hexCodex = '1234567890ABCDEF'
+//   let color = ''
+
+//   for (let i = 0; i < 6; i++) {
+//     color += hexCodex[Math.floor(Math.random() * hexCodex.length)]
+//   }
+
+//   return '#' + color
+// }
