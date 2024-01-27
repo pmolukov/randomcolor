ad786cb0c1f51a699ea5aafa31c40dc4dcd19711
Pavel Molukov <p.molukov@gmail.com>
2024-01-27 21:21:03 +0300

    remove data-type from i

--- script.js
+++ script.js
@@ -11,13 +11,8 @@ document.addEventListener("click", (event) => {
   const type = event.target.dataset.type;
 
   if (type === "lock") {
-    const node =
-      event.target.tagName.toLowerCase() === "i"
-        ? event.target
-        : event.target.children[0];
-
-    node.classList.toggle("fa-lock-open");
-    node.classList.toggle("fa-lock");
+    event.target.children[0].classList.toggle("fa-lock-open");
+    event.target.children[0].classList.toggle("fa-lock");
   } else if (type === "copy") {
     copyToClickboard(event.target.textContent);
   } else if (type === "change") {
